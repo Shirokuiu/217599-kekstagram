142fc1109f174b6fe26af261ff8c2529522dbb3c
Pavel <shirokuiuna@mail.ru>
2017-08-16 23:15:59 +0300

    add module3-task1

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var pictures = [];
-
 var userComments = [
   'Всё отлично!', 
   'В целом всё неплохо. Но не всё.', 
@@ -10,14 +9,47 @@ var userComments = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+var picturesContainer = document.querySelector('.pictures');
+var pictureTemplate = document.querySelector('#picture-template').content.querySelector('.picture');
+var fragment = document.createDocumentFragment();
+var galleryOverlay = document.querySelector('.gallery-overlay');
 
-for (var i = 0; i < 25; i++) {
-  pictures.push({
-    "url": ('photos/' + (Math.floor((Math.random() * (25 - 1 + 1))) + 1) + '.jpg'),
-    "likes": Math.floor((Math.random() * (200 - 1 + 1)) + 1),
-    "comments": userComments[Math.floor((Math.random() * (userComments.length - 1)) + 1)]
-  });
-}
+var calculateRandom = function (from, to) {
+  var random = Math.floor((Math.random() * (to - from + 1)) + from);
+  
+  return random;
+};
 
-console.log(pictures);
-console.log(userComments);
+var addData = function () {
+  var pushPictures = pictures;
+  
+  for(var i = 0; i < 25; i++) {
+    pushPictures.push({
+      "url": ('photos/' + calculateRandom(1, 25) + '.jpg'),
+      "likes": calculateRandom(15, 200),
+      "comments": userComments[calculateRandom(userComments.length - userComments.length, userComments.length - 1)]
+    });
+  };
+  
+  return pushPictures;
+};
+
+var createDomElements = function (container) {
+  for (var i = 0; i < pictures.length; i++) {
+    var createElement = pictureTemplate.cloneNode(true);
+    createElement.querySelector('img').setAttribute('src', pictures[i].url);
+    createElement.querySelector('.picture-comments').textContent = pictures[i].comments;
+    createElement.querySelector('.picture-likes').textContent = pictures[i].likes;
+
+    fragment.appendChild(createElement);
+  }
+  
+  return container.appendChild(fragment);
+};
+
+addData();
+createDomElements(picturesContainer);
+galleryOverlay.classList.remove('hidden');
+galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', pictures[0].url);
+galleryOverlay.querySelector('.likes-count').textContent = pictures[0].likes;
+galleryOverlay.querySelector('.comments-count').textContent = pictures[0].comments;
